5ee80df29a2008d40572654a0f901eff90b29e4b
Isabella <isabella.y.hu@gmail.com>
2022-10-11 20:34:45 +1100

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:name]
+      # @first_name = params[:name]
+      # @last_name = params[:name]
+      students = Student.where('lower(first_name)=? OR lower(last_name)=?',params[:name].downcase, params[:name].downcase)
+    else
+      students = Student.all
+    end
+      render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
